6d9a7a1b888b7f387249dcff0e1e2bbd0c40f935
Venkatesh <k.venkatesh.rules@gmail.com>
2023-10-09 13:55:30 +0530

    change response to hello mummy

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import { createServer } from "node:http";
 
 const server = createServer((req, res) => {
   console.log(process.env);
-  res.end("hello!");
+  res.end("hello mummy K.B.T.Sundari devi from Venkatesh!");
 });
 
 server.listen(process.env.PORT || 3000);
