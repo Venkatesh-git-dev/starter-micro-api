45b2df3dd38b9d7fcca93df48bf28c92b0aaa67a
Venkatesh <k.venkatesh.rules@gmail.com>
2023-10-09 13:50:52 +0530

    test for env variables

--- server.mjs
+++ server.mjs
@@ -1,5 +1,8 @@
 import { createServer } from "node:http";
 
-const server = createServer((req, res) => res.end("hello!"));
+const server = createServer((req, res) => {
+  console.log(process.env);
+  res.end("hello!");
+});
 
 server.listen(process.env.PORT || 3000);
