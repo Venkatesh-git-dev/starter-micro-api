d78be44ad056a50c930ded88d3fffcbb102f1b97
Venkatesh <k.venkatesh.rules@gmail.com>
2023-10-09 14:04:55 +0530

    increased the font size

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import { createServer } from "node:http";
 
 const server = createServer((req, res) => {
   console.log(process.env);
-  res.end("hello mummy K.B.T.Sundari devi from Venkatesh!");
+  res.end("<h1>hello mummy K.B.T.Sundari devi from Venkatesh!</h1>");
 });
 
 server.listen(process.env.PORT || 3000);
